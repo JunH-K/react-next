41c19abebb3ab0fd502dc8c7665572318f19b007
JunH-K <k.developer@kakao.com>
2020-06-24 20:50:08 +0900

    feat : add  Component of pages

---
+++ front/pages/_app.js
@@ -0,0 +1,21 @@
+import React from 'react'; //next 는 import 안해도됨
+import PropTypes from 'prop-types';
+import 'antd/dist/antd.css';
+
+/* pages 컴포넌트들의 공통 요소 페이지*/
+
+/**
+ *
+ * @param Component pages 에 있는 컴포넌트 들이 props 로 들어온다.
+ * @returns {*}
+ * @constructor
+ */
+const App = ({ Component }) => {
+  return <Component />;
+};
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+};
+
+export default App;
